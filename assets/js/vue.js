7d2eeea9825bce15ec7d1c411be4069ae94085da
Mark Honeychurch <mark@honeychurch.org>
2021-10-22 02:21:48 +1300

    Added URL hash

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,6 +1,8 @@
 import {createApp} from 'https://unpkg.com/petite-vue?module';
 import names from './names.js';
+console.log(location.hash.substr(1));
+const name = names[location.hash.substr(1)] ? location.hash.substr(1) : 'Sarah';
 createApp({
-	name: 'Sarah',
+	name,
 	names,
 }).mount();
