01df82acce29857aca6c904a494fa2c92b5e281b
Mark Honeychurch <mark@honeychurch.org>
2021-10-21 22:48:43 +1300

    Fixed URL

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,4 +1,4 @@
-import {createApp} from 'https://unpkg.com/petite-vue?module'
+import {createApp} from 'https://unpkg.com/petite-vue?module';
 import names from './names.js';
 createApp({
 	name: 'Sarah',
