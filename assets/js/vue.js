0f32b0efc592645608f3971ea0b172ef96c1ef83
Mark Honeychurch <mark.honeychurch@telesmart.co.nz>
2021-10-22 16:15:50 +1300

    Fixed select sizing and outline

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -4,14 +4,9 @@ const name = names[location.hash.substr(1)] ? location.hash.substr(1) : 'Sarah';
 createApp({
 	name,
 	names,
-	get width() {
-		if (this.name) {
-			nextTick(() => {
-				if (this.$refs && this.$refs.name) return this.$refs.name.clientWidth + 20;
-				else return 170;
-			});
-		} else {
-			return 170;
-		}
+	width: 135,
+	async change() {
+		location = '#' + name;
+		this.width = this.$refs.name.clientWidth + 20;
 	},
 }).mount();
