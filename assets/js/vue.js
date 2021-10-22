7ec7f57ef9d888bdcebefc9aa33e52188983305c
Mark Honeychurch <mark.honeychurch@telesmart.co.nz>
2021-10-22 14:03:46 +1300

    Attempt to get dynamic width

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,7 +1,17 @@
-import {createApp} from 'https://unpkg.com/petite-vue?module';
+import {createApp, nextTick} from 'https://unpkg.com/petite-vue?module';
 import names from './names.js';
 const name = names[location.hash.substr(1)] ? location.hash.substr(1) : 'Sarah';
 createApp({
 	name,
 	names,
+	get width() {
+		if (this.name) {
+			nextTick(() => {
+				if (this.$refs && this.$refs.name) return this.$refs.name.clientWidth + 20;
+				else return 170;
+			});
+		} else {
+			return 170;
+		}
+	},
 }).mount();
