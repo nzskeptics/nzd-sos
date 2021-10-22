8e6b39955d2a3d9f3d7dd756365f0d962ac9d679
Mark Honeychurch <mark.honeychurch@telesmart.co.nz>
2021-10-22 13:21:02 +1300

    Almost have dropdown resizing working

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,6 +1,5 @@
 import {createApp} from 'https://unpkg.com/petite-vue?module';
 import names from './names.js';
-console.log(location.hash.substr(1));
 const name = names[location.hash.substr(1)] ? location.hash.substr(1) : 'Sarah';
 createApp({
 	name,
